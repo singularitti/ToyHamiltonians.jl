c30906dcb7c714895d60511f82a9b84f0782262f
singularitti <singularitti@outlook.com>
2024-10-12 02:21:11 -0600

    Deprecate `rand_uniform` & `rand_normal`, use `rand(RandomEigen, ...)`

--- src/distributions.jl
+++ src/distributions.jl
@@ -12,14 +12,4 @@ struct RandomEigen{D<:Distribution,T,R<:AbstractRNG}
 end
 RandomEigen(dist, type=Float64, rng=default_rng()) = RandomEigen(dist, type, rng)
 
-rand_uniform(rng::AbstractRNG, ::Type{X}, dims::Integer...) where {X} =
-    rand(rng, X, Uniform(0, 1), dims...) .* oneunit(X)
-rand_uniform(::Type{X}, dims::Integer...) where {X} =
-    rand(Uniform(0, 1), dims...) .* oneunit(X)
-rand_uniform(dims::Integer...) = rand(Uniform(0, 1), dims...)
-
-rand_normal(rng::AbstractRNG, ::Type{X}, μ, σ, dims::Integer...) where {X} =
-    rand(rng, X, Normal(μ, σ), dims...) .* oneunit(X)
-rand_normal(::Type{X}, μ, σ, dims::Integer...) where {X} =
-    rand(Normal(μ, σ), dims...) .* oneunit(X)
-rand_normal(μ, σ, dims::Integer...) = rand(Normal(μ, σ), dims...)
+rand(x::RandomEigen, dims::Integer...) = rand(x.rng, x.dist, dims...) .* oneunit(x.type)
