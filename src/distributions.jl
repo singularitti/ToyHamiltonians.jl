23fa7cdb8c88d39616b09222fed4866b81fef49a
singularitti <singularitti@outlook.com>
2024-10-12 17:44:54 -0600

    Add `EigvecsSampler` & `rand` from `EigvecsSampler`

--- src/distributions.jl
+++ src/distributions.jl
@@ -1,9 +1,10 @@
 using Distributions: Distribution, Truncated, Uniform, Normal, truncated
+using LinearAlgebra: qr
 using Random: AbstractRNG, Sampler, default_rng
 
 import Random: rand
 
-export EigvalsSampler
+export EigvalsSampler, EigvecsSampler
 
 struct EigvalsSampler{R<:AbstractRNG,T,D<:Truncated} <: Sampler{T}
     rng::R
@@ -16,4 +17,17 @@ struct EigvalsSampler{R<:AbstractRNG,T,D<:Truncated} <: Sampler{T}
     end
 end
 
+struct EigvecsSampler{R<:AbstractRNG,T,D<:Distribution} <: Sampler{T}
+    rng::R
+    type::T
+    dist::D
+    EigvecsSampler(dist, type=Float64, rng=default_rng()) =
+        new{typeof(rng),typeof(type),typeof(dist)}(rng, type, dist)
+end
+
 rand(s::EigvalsSampler, dims::Integer...) = rand(s.rng, s.dist, dims...) .* oneunit(s.type)
+function rand(s::EigvecsSampler, m::Integer, n::Integer)
+    matrix = rand(s.rng, s.dist, m, n) .* oneunit(s.type)
+    q, _ = qr(matrix)
+    return q
+end
