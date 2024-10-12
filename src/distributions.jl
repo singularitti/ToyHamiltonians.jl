2910b0cc7fe52fdca479d717c0b6c413b8686e90
singularitti <singularitti@outlook.com>
2024-10-12 17:18:27 -0600

    Redefine `EigvalsSampler` using `Truncated`

--- src/distributions.jl
+++ src/distributions.jl
@@ -1,15 +1,19 @@
-using Distributions: Distribution, Uniform, Normal
-using Random: AbstractRNG, default_rng
+using Distributions: Distribution, Truncated, Uniform, Normal, truncated
+using Random: AbstractRNG, Sampler, default_rng
 
 import Random: rand
 
 export EigvalsSampler
 
-struct EigvalsSampler{D<:Distribution,T,R<:AbstractRNG}
-    dist::D
-    type::T
+struct EigvalsSampler{R<:AbstractRNG,T,D<:Truncated} <: Sampler{T}
     rng::R
+    type::T
+    dist::D
+    function EigvalsSampler(dist::Distribution, type=Float64, rng=default_rng())
+        lower, upper = zero(type), one(type)
+        dist = truncated(dist; lower=lower, upper=upper)
+        return new{typeof(rng),typeof(type),typeof(dist)}(rng, type, dist)
+    end
 end
-EigvalsSampler(dist, type=Float64, rng=default_rng()) = EigvalsSampler(dist, type, rng)
 
 rand(s::EigvalsSampler, dims::Integer...) = rand(s.rng, s.dist, dims...) .* oneunit(s.type)
